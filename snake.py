5f49a896e6f7fa78af7432b1e9688ea398120f3f
Scott Clarke <scott.clarke@student.manchester.ac.uk>
2018-09-19 15:21:56 +0100

    About to mess with movement logic

--- snake.py
+++ snake.py
@@ -48,71 +48,75 @@ elif corner == 3:
     direction = 3
   sense.set_pixel(xDelta(7, direction), yDelta(7, direction), red)
 
-def move_down(event):
-  if event.action == 'pressed':
-    pixel_list = sense.get_pixels()
-    pixel_number = 0
-    for pixel in pixel_list:
-      if pixel != [0, 0, 0]:
-        pixel_y_x = divmod(pixel_number, 8)
-        if pixel_y_x[0] == 7:
-          sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
-          sense.set_pixel(pixel_y_x[1], 0, white)
-        else:
-          sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
-          sense.set_pixel(pixel_y_x[1], pixel_y_x[0] + 1, white)
+def move_down():
+  pixel_list = sense.get_pixels()
+  pixel_number = 0
+  for pixel in pixel_list:
+    if pixel != [0, 0, 0]:
+      pixel_y_x = divmod(pixel_number, 8)
+      if pixel_y_x[0] == 7:
+        sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
+        sense.set_pixel(pixel_y_x[1], 0, white)
       else:
-        pixel_number += 1
+        sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
+        sense.set_pixel(pixel_y_x[1], pixel_y_x[0] + 1, white)
+    else:
+      pixel_number += 1
 
-def move_up(event):
-  if event.action == 'pressed':
-    pixel_list = sense.get_pixels()
-    pixel_number = 0
-    for pixel in pixel_list:
-      if pixel != [0, 0, 0]:
-        pixel_y_x = divmod(pixel_number, 8)
-        if pixel_y_x[0] == 0:
-          sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
-          sense.set_pixel(pixel_y_x[1], 7, white)
-        else:
-          sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
-          sense.set_pixel(pixel_y_x[1], pixel_y_x[0] - 1, white)
+def move_up():
+  pixel_list = sense.get_pixels()
+  pixel_number = 0
+  for pixel in pixel_list:
+    if pixel != [0, 0, 0]:
+      pixel_y_x = divmod(pixel_number, 8)
+      if pixel_y_x[0] == 0:
+        sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
+        sense.set_pixel(pixel_y_x[1], 7, white)
       else:
-        pixel_number += 1
+        sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
+        sense.set_pixel(pixel_y_x[1], pixel_y_x[0] - 1, white)
+    else:
+      pixel_number += 1
     
-def move_right(event):
-  if event.action == 'pressed':
-    pixel_list = sense.get_pixels()
-    pixel_number = 0
-    for pixel in pixel_list:
-      if pixel != [0, 0, 0]:
-        pixel_y_x = divmod(pixel_number, 8)
-        if pixel_y_x[1] == 7:
-          sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
-          sense.set_pixel(0, pixel_y_x[0], white)
-        else:
-          sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
-          sense.set_pixel(pixel_y_x[1] + 1, pixel_y_x[0], white)
+def move_right():
+  pixel_list = sense.get_pixels()
+  pixel_number = 0
+  for pixel in pixel_list:
+    if pixel != [0, 0, 0]:
+      pixel_y_x = divmod(pixel_number, 8)
+      if pixel_y_x[1] == 7:
+        sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
+        sense.set_pixel(0, pixel_y_x[0], white)
       else:
-        pixel_number += 1
+        sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
+        sense.set_pixel(pixel_y_x[1] + 1, pixel_y_x[0], white)
+    else:
+      pixel_number += 1
 
-def move_left(event):
-  if event.action == 'pressed':
-    pixel_list = sense.get_pixels()
-    pixel_number = 0
-    for pixel in pixel_list:
-      if pixel != [0, 0, 0]:
-        pixel_y_x = divmod(pixel_number, 8)
-        if pixel_y_x[1] == 0:
-          sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
-          sense.set_pixel(7, pixel_y_x[0], white)
-        else:
-          sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
-          sense.set_pixel(pixel_y_x[1] - 1, pixel_y_x[0], white)
+def move_left():
+  pixel_list = sense.get_pixels()
+  pixel_number = 0
+  for pixel in pixel_list:
+    if pixel != [0, 0, 0]:
+      pixel_y_x = divmod(pixel_number, 8)
+      if pixel_y_x[1] == 0:
+        sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
+        sense.set_pixel(7, pixel_y_x[0], white)
       else:
-        pixel_number += 1
+        sense.set_pixel(pixel_y_x[1], pixel_y_x[0], blank)
+        sense.set_pixel(pixel_y_x[1] - 1, pixel_y_x[0], white)
+    else:
+      pixel_number += 1
 
 sense.stick.direction_any = change_direction
 
 while True:
-  
+  sleep(0.25)
+  if direction == 0:
+    move_up()
+  elif direction == 1:
+    move_right()
+  elif direction == 2:
+    move_down()
+  elif direction == 3:
+    move_left()
